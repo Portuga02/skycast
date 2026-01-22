68683aaf228768cf074cd06f40333eb061d1afae
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2026-01-22 14:45:34 -0300

    ajuste e criação de gelolização em tempo real

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
+
 use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
 // Rota para o Clima Final
+Route::get('/clima/coordenadas', [WeatherController::class, 'getByCoordinates']);
 Route::get('/clima/{city}', [WeatherController::class, 'getClima']);
-   
+
 
 // Rota para as Sugestões (Autocomplete)
-Route::get('/cidades/busca/{query}', [WeatherController::class, 'search'])
-     ->where('query', '.*');
+Route::get('/cidades/busca/{query}', [WeatherController::class, 'search'])->where('query', '.*');
