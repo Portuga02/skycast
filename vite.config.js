99f95fdb5f7f15dafdbe788d7be59e0399d9716a
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2026-04-02 11:40:05 -0300

    COLOCANDO CSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'; // <--- Garanta que o nome do pacote está completo
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css', // Adicionamos o CSS aqui!
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         vue(),
